3fed410f566eaea4bdb8ece85771b280916438b8
istvanbodor <istvanbodor@users.noreply.github.com>
2022-09-20 10:56:23 +0000

    deploy: f35f318c4c25c7499a9c73ed22f2e90b59a2cd77

---
+++ bmi.js
@@ -0,0 +1,74 @@
+function klikk()
+{
+
+    let index = parseFloat(document.getElementById('suly').value / (document.getElementById('magassag').value/100)**2).toFixed(1);
+
+    document.getElementById('ertek').innerHTML = "AZ ÖN BMI ÉRTÉKE: "+ index;
+
+    if (index<parseFloat(18,5))
+    {
+        document.getElementById('sovany').style.backgroundColor ="orange";
+        document.getElementById('normal').style.backgroundColor ="white";
+        document.getElementById('tulsuly').style.backgroundColor ="white";
+        document.getElementById('egyelhizas').style.backgroundColor ="white";
+        document.getElementById('ketelhizas').style.backgroundColor ="white";
+        document.getElementById('haromelhizas').style.backgroundColor ="white";
+    }
+    else if (index <25)
+    {
+        document.getElementById('sovany').style.backgroundColor ="white";
+        document.getElementById('normal').style.backgroundColor ="orange";
+        document.getElementById('tulsuly').style.backgroundColor ="white";
+        document.getElementById('egyelhizas').style.backgroundColor ="white";
+        document.getElementById('ketelhizas').style.backgroundColor ="white";
+        document.getElementById('haromelhizas').style.backgroundColor ="white";
+    }
+    else if (index <30)
+    {
+        document.getElementById('sovany').style.backgroundColor ="white";
+        document.getElementById('normal').style.backgroundColor ="white";
+        document.getElementById('tulsuly').style.backgroundColor ="orange";
+        document.getElementById('egyelhizas').style.backgroundColor ="white";
+        document.getElementById('ketelhizas').style.backgroundColor ="white";
+        document.getElementById('haromelhizas').style.backgroundColor ="white";
+    }
+    else if (index <35)
+    {
+        document.getElementById('sovany').style.backgroundColor ="white";
+        document.getElementById('normal').style.backgroundColor ="white";
+        document.getElementById('tulsuly').style.backgroundColor ="white";
+        document.getElementById('egyelhizas').style.backgroundColor ="orange";
+        document.getElementById('ketelhizas').style.backgroundColor ="white";
+        document.getElementById('haromelhizas').style.backgroundColor ="white"; 
+    }
+    else if (index <40)
+    {
+        document.getElementById('sovany').style.backgroundColor ="white";
+        document.getElementById('normal').style.backgroundColor ="white";
+        document.getElementById('tulsuly').style.backgroundColor ="white";
+        document.getElementById('egyelhizas').style.backgroundColor ="white";
+        document.getElementById('ketelhizas').style.backgroundColor ="orange";
+        document.getElementById('haromelhizas').style.backgroundColor ="white";
+    }
+    else {
+        document.getElementById('sovany').style.backgroundColor ="white";
+        document.getElementById('normal').style.backgroundColor ="white";
+        document.getElementById('tulsuly').style.backgroundColor ="white";
+        document.getElementById('egyelhizas').style.backgroundColor ="white";
+        document.getElementById('ketelhizas').style.backgroundColor ="white";
+        document.getElementById('haromelhizas').style.backgroundColor ="orange";
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+document.getElementById('szamitogomb').addEventListener('click',klikk);
